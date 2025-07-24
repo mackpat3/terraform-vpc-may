ea3fe51e4effd0b192cbc4b28a06704c29adb18b
mackpat3 <mackpat3@gmail.com>
2025-07-24 00:02:38 +0000

    changes vpc

--- main.tf
+++ main.tf
@@ -5,8 +5,9 @@ provider aws {
 
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
+  
   tags = {
-    Name = "my-vpc"
+    Name = var.vpc_name
   }
 }
 
--- variables.tf
+++ variables.tf
@@ -1,4 +1,5 @@
 variable vpc_cidr {}
 variable subnet1_cidr {}
-variable subnet3_cidr {}
 variable subnet2_cidr {}
+variable subnet3_cidr {}
+variable vpc_name {}
